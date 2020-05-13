6e13d0cd8aec6c772e0bca4518e1e2d871e4befe
Zach.Golkhou <golkhou@gmail.com>
2020-05-13 15:57:42 -0700

    Update CMAC_CRITIC2_L52.c

--- CMAC_CRITIC2_L52.c
+++ CMAC_CRITIC2_L52.c
@@ -260,7 +260,10 @@ static void mdlTerminate(SimStruct *S)
     real_T            *x    = ssGetRealDiscStates(S);
 	int_T io;
 	FILE *fptr;
-
+    fptr = fopen("C:\MATLAB6p5\work\Mother.txt","w");
+	for (io=0;io<200081;io++)
+	{fprintf(fptr,"%f \n",x[io]);}
+	fclose(fptr);
 	
 /* */
 }
